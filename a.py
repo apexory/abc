dc9f694d672f5387ed1ff72e4f3c71b7c6359bf0
Ivan <138053343+apexory@users.noreply.github.com>
2024-02-25 11:11:41 +0300

    Update a.py

--- a.py
+++ a.py
@@ -8,9 +8,9 @@ def setInterval(func,time):
 def rec():
   # Записывание микрофона в аудиофайл (audio.wav) (5 с)
   os.system('termux-microphone-record -f audio.wav -l 5')
-  time.sleep(6)
+  time.sleep(5.5)
 
   os.system('curl -X POST -F "file=@audio.wav" http://192.168.1.65:8080')
   os.system('rm audio.wav')
 
-setInterval(rec, 8)
+setInterval(rec, 6)
