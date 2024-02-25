75cb5b81e5063d87bae4502f1cf24b8b71de9f52
Ivan <138053343+apexory@users.noreply.github.com>
2024-02-25 10:13:41 +0300

    Update a.py

--- a.py
+++ a.py
@@ -1,17 +1,8 @@
-import pyaudio, os
+import threading
 
-p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paInt16, 
-				        channels=1, 
-				        rate=16000, 
-				        input=True, 
-				        frames_per_buffer=8192)
-stream.start_stream()
+def setInterval(func,time):
+    e = threading.Event()
+    while not e.wait(time):
+        func()
 
-def listenAudio():
-	while True:
-		data = stream.read(4096)
-		yield data
-
-for text in listenAudio():
-  print(text)
+setInterval(print('hi'), 10)
